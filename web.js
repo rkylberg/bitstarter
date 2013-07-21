d9d0b9b2975f6d5f6d5936da34a08ee0d9b8dd90
Robert Kylberg <rkylberg@gmail.com>
2013-07-21 15:56:11 -0700

    Loading response message from indext.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var text =  fs.readFileSync('index.html', 'utf8');
+	response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
